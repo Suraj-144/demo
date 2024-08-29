a91ad2283f7f045266dd450728fe7165de61cdbd
suraj-144 <raajsuraj144@gmail.com>
2024-08-30 02:08:13 +0530

    added responsebody in controller

--- src/main/java/com/Test/demo_test/controller/SayHi.java
+++ src/main/java/com/Test/demo_test/controller/SayHi.java
@@ -4,14 +4,18 @@ package com.Test.demo_test.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+
 @RequestMapping("/say")
 public class SayHi {
 
+    // http://loaclhost:8080/say/get
 
     @GetMapping("/get")
+    @ResponseBody
     public String sayHi(){
         return "Hii";
     }
